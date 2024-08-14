3e566c47f6b76e9d41051591f40d7e449534fadf
David Hafner <david.hafner@tingo.ch>
2024-08-14 15:16:49 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="w-full justify-center flex pt-20">
+		<details class="dropdown">
+			<summary class="btn m-1">open or close</summary>
+			<ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+				<li v-for="route of routes">
+					<nuxt-link :to="route.path">{{ route.name }}</nuxt-link>
+				</li>
+			</ul>
+		</details>
+	</div>
+	<div class="w-full justify-center items-center flex pt-20 flex-col"><NuxtPage /></div>
+</template>
+<script setup lang="ts">
+const router = useRouter();
+const routes = router.getRoutes();
+</script>
---
+++ pages/morsecode.vue
@@ -0,0 +1,63 @@
+<template>
+	<input @input="encode" type="text" placeholder="Your message to encode" class="input input-bordered w-full max-w-xs my-2" ref="cleartext" />
+	<input @input="decode" type="text" placeholder="Your message to decode" class="input input-bordered w-full max-w-xs my-2" ref="encoded" />
+</template>
+
+<script lang="ts" setup>
+const cleartext = ref<null | HTMLInputElement>(null);
+const encoded = ref<null | HTMLInputElement>(null);
+const values = {
+	"a": ".-",
+	"b": "-...",
+	"c": "-.-.",
+	"d": "-..",
+	"e": ".",
+	"f": "..-.",
+	"g": "--.",
+	"h": "....",
+	"i": "..",
+	"j": ".---",
+	"k": "-.-",
+	"l": ".-..",
+	"m": "--",
+	"n": "-.",
+	"o": "---",
+	"p": ".--.",
+	"q": "--.-",
+	"r": ".-.",
+	"s": "...",
+	"t": "-",
+	"u": "..-",
+	"v": "...-",
+	"w": ".--",
+	"x": "-..-",
+	"y": "-.--",
+	"z": "--..",
+};
+
+const encodedValues = {};
+for (const encodedValue in values) {
+	encodedValues[values[encodedValue]] = encodedValue;
+}
+
+const encode = () => {
+	encoded.value.value = "";
+	cleartext.value.value
+		.toLowerCase()
+		.split("")
+		.forEach((char) => {
+			encoded.value.value += values[char] + " ";
+		});
+};
+const decode = () => {
+	cleartext.value.value = "";
+	encoded.value.value
+		.toLowerCase()
+		.split(" ")
+		.forEach((char) => {
+			cleartext.value.value += encodedValues[char];
+		});
+};
+</script>
+
+<style></style>
