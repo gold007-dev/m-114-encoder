f6fe76ab6f2a4b9ad6e2366758974b56662eb608
Gold007 <=>
2024-08-15 14:37:50 +0200

    subdomain set in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
 
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss"]
+    modules: ["@nuxtjs/tailwindcss"],
+    app:{
+        baseURL:"/m-114-encoder/"
+    }
 });
